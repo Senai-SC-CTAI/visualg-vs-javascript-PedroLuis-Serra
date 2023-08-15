2b048b3bd190746b3fe5b6f06a4c8858813c6524
PedroLuis-Serra <131630260+PedroLuis-Serra@users.noreply.github.com>
2023-08-15 09:03:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrar(){
+    var totalFilho = 200/4
+    var filhosPorDecada = totalFilho/20
+
+    document.getElementById("r").innerHTML = ("O números de filhotes que terá ao longo da vida é de: "+ totalFilho);
+    document.getElementById("r2").innerHTML = (" A média de filhotes por decada é de: "+filhosPorDecada) ;
+
+
+}
